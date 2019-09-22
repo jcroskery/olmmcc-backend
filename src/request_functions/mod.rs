af3bba66622bea2f2733f2547e02430fbe533564
Justus Croskery <justus@olmmcc.tk>
2019-09-22 18:49:35 -0400

    Another diagnostic

--- src/request_functions/mod.rs
+++ src/request_functions/mod.rs
@@ -478,8 +478,8 @@ pub fn send_gmail_code(body: HashMap<&str, &str>) -> String {
             let request =
                 http::make_secure_request("https://www.googleapis.com/oauth2/v4/token", hash);
             let request_json: Value = serde_json::from_str(&request).unwrap();
-            println!("{:?}", request_json);
             let refresh_token = request_json["refresh_token"].as_str().unwrap();
+            println!("{:?}", session.get("email"));
             let email = &session.get("email").unwrap();
             if row_exists("admin", "email", email) {
                 change_row_where("admin", "email", email, "refresh_token", refresh_token);
