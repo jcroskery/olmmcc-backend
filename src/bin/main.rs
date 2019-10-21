e49bdf3a9c86330f78f65e517dba85e9d24cc204
Justus Croskery <justus@olmmcc.tk>
2019-10-20 20:30:27 -0400

    Add num_cpus calculation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,11 +2,9 @@ use threadpool::ThreadPool;
 
 use std::net::TcpListener;
 
-const NUM_THREADS: usize = 4;
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
-    let pool = ThreadPool::new(NUM_THREADS);
+    let pool = ThreadPool::new(num_cpus::get());
     for stream in listener.incoming() {
         pool.execute(move || {
             olmmcc::handle_connection(stream.unwrap());
